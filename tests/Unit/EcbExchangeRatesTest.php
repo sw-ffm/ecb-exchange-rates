32299735cea837dc637f7f8310db29fa7a22f1dc
Stefan Watzinger <stefanwatzingerfrankfurt@gmail.com>
2024-07-31 18:59:51 +0200

    test update

--- tests/Unit/EcbExchangeRatesTest.php
+++ tests/Unit/EcbExchangeRatesTest.php
@@ -2,22 +2,22 @@
 
 use EcbExchangeRates\EcbExchangeRates;
 $GLOBALS['ezb_rates'] = new EcbExchangeRates;
+
+
+it('Data array has exchange rate values', function () {
     
-
-it('json data has exchange rate values', function () {
-
-     $this->assertJson($GLOBALS['ezb_rates']->exchange_rates_json);
-
-});
-
-it('array has exchange rate values', function () {
-
     $this->assertIsArray($GLOBALS['ezb_rates']->exchange_rates_array);
-
+    
 });
 
-it('exchange rate data contain USD data', function () {
-
+it('Exchange rate data contain US-Dollar rate', function () {
+    
     $this->assertArrayHasKey('USD', $GLOBALS['ezb_rates']->exchange_rates_array);
+    
+});
+
+it('JSON data has exchange rate values', function () {
+
+    $this->assertJson($GLOBALS['ezb_rates']->exchange_rates_json);
 
 });
